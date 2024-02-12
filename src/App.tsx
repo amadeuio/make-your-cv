3dc921c1d60736fe28c19723accc6ae5db3bdcb8
nightrunner4 <118369464+nightrunner4@users.noreply.github.com>
2024-02-12 10:48:25 +0100

    Update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-function PersonalInfoForm() {
+function HeaderForm() {
   return (
-    <form className="personal-info-form">
+    <form className="header-form">
       <fieldset>
-        <legend>Personal Info</legend>
+        <legend>Header</legend>
         <div className="input-container">
-          <label htmlFor="fullName">Full Name:</label>
-          <input type="text" id="fullName" name="fullName" />
+          <label htmlFor="fullName">Full Name</label>
+          <input type="text" id="fullName" name="fullName" placeholder="Enter your full name" />
         </div>
         <div className="input-container">
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" />
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" placeholder="Enter your email address" />
         </div>
         <div className="input-container">
-          <label htmlFor="phone">Phone:</label>
-          <input type="tel" id="phone" name="phone" />
+          <label htmlFor="phone">Phone</label>
+          <input type="tel" id="phone" name="phone" placeholder="Enter your phone number" />
         </div>
         <div className="input-container">
-          <label htmlFor="adress">Adress:</label>
-          <input type="text" id="adress" name="adress" />
+          <label htmlFor="address">Address</label>
+          <input type="text" id="address" name="address" placeholder="Enter your address" />
         </div>
       </fieldset>
     </form>
@@ -30,23 +30,23 @@ function EducationForm() {
       <fieldset>
         <legend>Education</legend>
         <div className="input-container">
-          <label htmlFor="school">School:</label>
+          <label htmlFor="school">School</label>
           <input type="text" id="school" name="school" />
         </div>
         <div className="input-container">
-          <label htmlFor="qualification">Qualification:</label>
+          <label htmlFor="qualification">Qualification</label>
           <input type="text" id="qualification" name="qualification" />
         </div>
         <div className="input-container">
-          <label htmlFor="startDate">Start Date:</label>
+          <label htmlFor="startDate">Start Date</label>
           <input type="date" id="startDate" name="startDate" />
         </div>
         <div className="input-container">
-          <label htmlFor="endDate">End Date:</label>
+          <label htmlFor="endDate">End Date</label>
           <input type="date" id="endDate" name="endDate" />
         </div>
         <div className="input-container">
-          <label htmlFor="location">Location:</label>
+          <label htmlFor="location">Location</label>
           <input type="text" id="location" name="location" />
         </div>
       </fieldset>
@@ -60,27 +60,27 @@ function ExperienceForm() {
       <fieldset>
         <legend>Experience</legend>
         <div className="input-container">
-          <label htmlFor="company">Company:</label>
+          <label htmlFor="company">Company</label>
           <input type="text" id="company" name="company" />
         </div>
         <div className="input-container">
-          <label htmlFor="position">Position:</label>
+          <label htmlFor="position">Position</label>
           <input type="text" id="position" name="position" />
         </div>
         <div className="input-container">
-          <label htmlFor="startDate">Start Date:</label>
+          <label htmlFor="startDate">Start Date</label>
           <input type="date" id="startDate" name="startDate" />
         </div>
         <div className="input-container">
-          <label htmlFor="endDate">End Date:</label>
+          <label htmlFor="endDate">End Date</label>
           <input type="date" id="endDate" name="endDate" />
         </div>
         <div className="input-container">
-          <label htmlFor="location">Location:</label>
+          <label htmlFor="location">Location</label>
           <input type="text" id="location" name="location" />
         </div>
         <div className="input-container">
-          <label htmlFor="description">Description:</label>
+          <label htmlFor="description">Description</label>
           <textarea id="description" name="description"></textarea>
         </div>
       </fieldset>
@@ -103,7 +103,7 @@ function CvContainer() {
 function Sidebar() {
   return (
     <aside className="sidebar">
-      <PersonalInfoForm />
+      <HeaderForm />
       <EducationForm />
       <ExperienceForm />
     </aside>
