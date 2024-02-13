da094fec2a17d6efdd2743068d52b025ea1bbc1e
nightrunner4 <118369464+nightrunner4@users.noreply.github.com>
2024-02-13 11:30:47 +0100

    Add CV header icons

--- src/components/HeaderCv.tsx
+++ src/components/HeaderCv.tsx
@@ -1,16 +1,24 @@
 import MailIcon from "../icons/MailIcon";
+import PhoneIcon from "../icons/PhoneIcon";
+import LocationIcon from "../icons/LocationIcon";
 
 function HeaderCv() {
   return (
     <div className="header-cv">
       <div className="header-cv-fullname">John Doe</div>
       <div className="header-cv-contact">
-        <div>
+        <div className="header-cv-contact-item">
           <MailIcon />
           johndoe@example.com
         </div>
-        <div>19028840</div>
-        <div>Utrecht, NL</div>
+        <div className="header-cv-contact-item">
+          <PhoneIcon />
+          19028840
+        </div>
+        <div className="header-cv-contact-item">
+          <LocationIcon />
+          Utrecht, NL
+        </div>
       </div>
     </div>
   );
