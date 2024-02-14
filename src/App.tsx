1adec251f48cecd7a355e133c168fdf9aeb1b2ce
nightrunner4 <118369464+nightrunner4@users.noreply.github.com>
2024-02-14 22:53:49 +0100

    Eliminate line break in description initial data

--- src/App.tsx
+++ src/App.tsx
@@ -103,8 +103,7 @@ const initialExperienceData: ExperienceData = {
   startDate: "Feb 2020",
   endDate: "Oct 2023",
   location: "Madrid, Spain",
-  description: `
-  - Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+  description: `- Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
 - Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
 - Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 `,
