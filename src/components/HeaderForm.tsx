4ac0f8a5c7b00c8dafe462af42cee5939e7354ce
nightrunner4 <118369464+nightrunner4@users.noreply.github.com>
2024-02-15 13:27:09 +0100

    Implement dropdown menu toggle functionality
    - Update dropdown menu to feature smooth expand and contract transitions for improved user experience
    - Refresh icon within the dropdown menu to reflect the new expand/contract functionality
    - Implement save button functionality to enable users to save changes, which essentially just contracts the menu
    - Introduce conditional rendering in the dropdown menu for enhanced UI responsiveness based on user interaction

--- src/components/HeaderForm.tsx
+++ src/components/HeaderForm.tsx
@@ -1,11 +1,17 @@
-import { useContext, ChangeEvent, FormEvent } from "react";
+import { useState, useContext, ChangeEvent, FormEvent } from "react";
 import { FormDataContext } from "../App";
 
 import Button from "./Button";
 import ExpandLessIcon from "../icons/ExpandLessIcon";
+import ExpandMoreIcon from "../icons/ExpandMoreIcon";
 
 function HeaderForm() {
   const { headerData, setHeaderData } = useContext(FormDataContext)!;
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -15,12 +21,11 @@ function HeaderForm() {
   const handleClear = (e: FormEvent<HTMLButtonElement>) => {
     e.preventDefault();
     setHeaderData({ fullName: "", email: "", phone: "", address: "" });
-    console.log("Clear detected:", headerData);
   };
 
   const handleSave = (e: FormEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    console.log("Save detected:", headerData);
+    toggleDropdown();
   };
 
   const { fullName, email, phone, address } = headerData;
@@ -28,57 +33,72 @@ function HeaderForm() {
   return (
     <form className="header-form">
       <fieldset>
-        <div className="legend-container">
-          <legend>Header</legend>
-          <ExpandLessIcon />
+        {/* Legend */}
+        <div className="legend-container" onClick={toggleDropdown}>
+          <div className="legend-content">
+            <legend>Header</legend>
+            {!isOpen && fullName && (
+              <div className="legend-fullname">
+                <span className="legend-dash">—</span>
+                {fullName}
+              </div>
+            )}
+          </div>
+          {isOpen ? <ExpandLessIcon /> : <ExpandMoreIcon />}
         </div>
-        <div className="input-container">
-          <label htmlFor="fullName">Full Name</label>
-          <input
-            type="text"
-            id="fullName"
-            name="fullName"
-            placeholder="Enter your full name"
-            value={fullName}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="input-container">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="Enter your email address"
-            value={email}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="input-container">
-          <label htmlFor="phone">Phone</label>
-          <input
-            type="tel"
-            id="phone"
-            name="phone"
-            placeholder="Enter your phone number"
-            value={phone}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="input-container">
-          <label htmlFor="address">Address</label>
-          <input
-            type="text"
-            id="address"
-            name="address"
-            placeholder="Enter your address"
-            value={address}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="button-container">
-          <Button className="clear-button" label="Clear" onClick={handleClear} />
-          <Button className="save-button" label="Save" onClick={handleSave} />
+
+        {/* Drop down menu */}
+        <div className={`dropdown-menu ${isOpen ? "open" : ""}`}>
+          <ul>
+            <li className="input-container">
+              <label htmlFor="fullName">Full Name</label>
+              <input
+                type="text"
+                id="fullName"
+                name="fullName"
+                placeholder="Enter your full name"
+                value={fullName}
+                onChange={handleChange}
+              />
+            </li>
+            <li className="input-container">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Enter your email address"
+                value={email}
+                onChange={handleChange}
+              />
+            </li>
+            <li className="input-container">
+              <label htmlFor="phone">Phone</label>
+              <input
+                type="tel"
+                id="phone"
+                name="phone"
+                placeholder="Enter your phone number"
+                value={phone}
+                onChange={handleChange}
+              />
+            </li>
+            <li className="input-container">
+              <label htmlFor="address">Address</label>
+              <input
+                type="text"
+                id="address"
+                name="address"
+                placeholder="Enter your address"
+                value={address}
+                onChange={handleChange}
+              />
+            </li>
+          </ul>
+          <div className="button-container">
+            <Button className="clear-button" label="Clear" onClick={handleClear} />
+            <Button className="save-button" label="Save" onClick={handleSave} />
+          </div>
         </div>
       </fieldset>
     </form>
