02447849880735a1e9b9930b2bdc169c0475bdb5
nightrunner4 <118369464+nightrunner4@users.noreply.github.com>
2024-02-19 09:08:07 +0100

    Refactor EducationForms
    The HTML element hierarchy has been changed for better organization, the vertical spacing of the elements has also been updated to adhere to the overall visual style of the page while ensuring the drag and drop works smoothly and consistenly

--- src/components/EducationForms.tsx
+++ src/components/EducationForms.tsx
@@ -64,118 +64,127 @@ function EducationForms() {
   };
 
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId="educationForms">
-        {(provided) => (
-          <div className="education-forms" {...provided.droppableProps} ref={provided.innerRef}>
-            <div className="form-title">Education</div>
+    <>
+      <div className="form-title">Education</div>
 
-            {educationArray.map((data, index) => (
-              <Draggable key={data.id} draggableId={data.id} index={index}>
-                {(provided) => (
-                  <div
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}>
-                    <form className="education-form">
-                      <fieldset>
-                        {/* Legend */}
-                        <div className="legend-container" onClick={() => toggleDropdown(data.id)}>
-                          <div className="legend-content">
-                            <legend>Education</legend>
-                            {!data.isOpen && data.qualification && (
-                              <div className="legend-qualification-preview">
-                                <span className="legend-dash">—</span>
-                                {data.qualification}
-                              </div>
-                            )}
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="educationFormsContainer">
+          {(provided) => (
+            <div
+              className="education-forms-container"
+              {...provided.droppableProps}
+              ref={provided.innerRef}>
+              {educationArray.map((data, index) => (
+                <Draggable key={data.id} draggableId={data.id} index={index}>
+                  {(provided) => (
+                    <div
+                      className="education-form-item"
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}>
+                      <form>
+                        <fieldset>
+                          {/* Legend */}
+                          <div className="legend-container" onClick={() => toggleDropdown(data.id)}>
+                            <div className="legend-content">
+                              <legend>Education</legend>
+                              {!data.isOpen && data.qualification && (
+                                <div className="legend-qualification-preview">
+                                  <span className="legend-dash">—</span>
+                                  {data.qualification}
+                                </div>
+                              )}
+                            </div>
+                            {data.isOpen ? <ExpandLessIcon /> : <ExpandMoreIcon />}
                           </div>
-                          {data.isOpen ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-                        </div>
 
-                        {/* Drop down menu */}
-                        <div className={`dropdown-menu ${data.isOpen ? "open" : ""}`}>
-                          <ul>
-                            <li className="input-container">
-                              <label htmlFor={`qualification_${data.id}`}>Qualification</label>
-                              <input
-                                type="text"
-                                id={`qualification_${data.id}`}
-                                name="qualification"
-                                placeholder="Enter the qualification name"
-                                value={data.qualification}
-                                onChange={(e) => handleChange(data.id, e)}
-                              />
-                            </li>
-                            <li className="input-container">
-                              <label htmlFor={`school_${data.id}`}>School</label>
-                              <input
-                                type="text"
-                                id={`school_${data.id}`}
-                                name="school"
-                                placeholder="Enter the school name"
-                                value={data.school}
-                                onChange={(e) => handleChange(data.id, e)}
-                              />
-                            </li>
-                            <li className="input-container">
-                              <label htmlFor={`location_${data.id}`}>Location</label>
-                              <input
-                                type="text"
-                                id={`location_${data.id}`}
-                                name="location"
-                                placeholder="Enter the location"
-                                value={data.location}
-                                onChange={(e) => handleChange(data.id, e)}
-                              />
-                            </li>
-                            <li className="input-container">
-                              <label htmlFor={`startDate_${data.id}`}>Start Date</label>
-                              <input
-                                type="text"
-                                id={`startDate_${data.id}`}
-                                name="startDate"
-                                placeholder="Enter the start date"
-                                value={data.startDate}
-                                onChange={(e) => handleChange(data.id, e)}
-                              />
-                            </li>
-                            <li className="input-container">
-                              <label htmlFor={`endDate_${data.id}`}>End Date</label>
-                              <input
-                                type="text"
-                                id={`endDate_${data.id}`}
-                                name="endDate"
-                                placeholder="Enter the end date"
-                                value={data.endDate}
-                                onChange={(e) => handleChange(data.id, e)}
-                              />
-                            </li>
-                          </ul>
+                          {/* Drop down menu */}
+                          <div className={`dropdown-menu ${data.isOpen ? "open" : ""}`}>
+                            <ul>
+                              <li className="input-container">
+                                <label htmlFor={`qualification_${data.id}`}>Qualification</label>
+                                <input
+                                  type="text"
+                                  id={`qualification_${data.id}`}
+                                  name="qualification"
+                                  placeholder="Enter the qualification name"
+                                  value={data.qualification}
+                                  onChange={(e) => handleChange(data.id, e)}
+                                />
+                              </li>
+                              <li className="input-container">
+                                <label htmlFor={`school_${data.id}`}>School</label>
+                                <input
+                                  type="text"
+                                  id={`school_${data.id}`}
+                                  name="school"
+                                  placeholder="Enter the school name"
+                                  value={data.school}
+                                  onChange={(e) => handleChange(data.id, e)}
+                                />
+                              </li>
+                              <li className="input-container">
+                                <label htmlFor={`location_${data.id}`}>Location</label>
+                                <input
+                                  type="text"
+                                  id={`location_${data.id}`}
+                                  name="location"
+                                  placeholder="Enter the location"
+                                  value={data.location}
+                                  onChange={(e) => handleChange(data.id, e)}
+                                />
+                              </li>
+                              <li className="input-container">
+                                <label htmlFor={`startDate_${data.id}`}>Start Date</label>
+                                <input
+                                  type="text"
+                                  id={`startDate_${data.id}`}
+                                  name="startDate"
+                                  placeholder="Enter the start date"
+                                  value={data.startDate}
+                                  onChange={(e) => handleChange(data.id, e)}
+                                />
+                              </li>
+                              <li className="input-container">
+                                <label htmlFor={`endDate_${data.id}`}>End Date</label>
+                                <input
+                                  type="text"
+                                  id={`endDate_${data.id}`}
+                                  name="endDate"
+                                  placeholder="Enter the end date"
+                                  value={data.endDate}
+                                  onChange={(e) => handleChange(data.id, e)}
+                                />
+                              </li>
+                            </ul>
 
-                          <div className="button-container">
-                            <button
-                              className="delete-button"
-                              onClick={(e) => handleDelete(data.id, e)}>
-                              Delete
-                            </button>
-                            <button className="save-button" onClick={(e) => handleSave(data.id, e)}>
-                              Save
-                            </button>
+                            <div className="button-container">
+                              <button
+                                className="delete-button"
+                                onClick={(e) => handleDelete(data.id, e)}>
+                                Delete
+                              </button>
+                              <button
+                                className="save-button"
+                                onClick={(e) => handleSave(data.id, e)}>
+                                Save
+                              </button>
+                            </div>
                           </div>
-                        </div>
-                      </fieldset>
-                    </form>
-                  </div>
-                )}
-              </Draggable>
-            ))}
-            {provided.placeholder}
-            <Button className="add-new-button" label={"+ Add new"} onClick={handleAddNew} />
-          </div>
-        )}
-      </Droppable>
-    </DragDropContext>
+                        </fieldset>
+                      </form>
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+
+      <Button className="add-new-button" label={"+ Add new"} onClick={handleAddNew} />
+    </>
   );
 }
 
